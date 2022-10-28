bfbf73160477b5b50df2a7de4ded585c953fbde4
Nick Raptis <nraptis@gmail.com>
2022-10-28 08:10:30 -0400

    initial commit

---
+++ HeapTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeapTesting
+//
+//  Created by Nicky Taylor on 10/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HeapTestingTests/MaxHeapTests.swift
@@ -0,0 +1,71 @@
+//
+//  HeapTestingTests.swift
+//  HeapTestingTests
+//
+//  Created by Nicky Taylor on 10/20/22.
+//
+
+import XCTest
+@testable import HeapTesting
+
+final class MaxHeapTests: XCTestCase {
+    
+    func testHeapExists() {
+        let heap = MaxHeap<Int>()
+    }
+    
+    func testHeapAddOneElement() {
+        var heap = MaxHeap<Int>()
+        heap.insert(5)
+        XCTAssert(heap.count == 1)
+    }
+    
+    func testHeapPopOneElement() {
+        var heap = MaxHeap<Int>()
+        heap.insert(5)
+        let top = heap.pop()!
+        XCTAssert(top == 5)
+    }
+    
+    func testHeapLotsOfStuff() {
+        
+        var sortedArray = [Int]()
+        var heap = MaxHeap<Int>()
+        
+        for _ in 0..<10000 {
+            
+            let random = Int.random(in: 0...10)
+            
+            if random < 7 {
+                //add something to heap and array
+                
+                var number = Int.random(in: 0...100)
+                sortedArray.append(number)
+                sortedArray.sort()
+                
+                heap.insert(number)
+                
+                XCTAssert(heap.peek()! == sortedArray.last!)
+                XCTAssert(heap.count == sortedArray.count)
+            } else {
+                if heap.count > 0 && sortedArray.count > 0 {
+                    let pop = heap.pop()!
+                    let arrback = sortedArray.popLast()!
+                    XCTAssert(pop == arrback)
+                    XCTAssert(heap.count == sortedArray.count)
+                }
+            }
+            
+            let killSwitch = Int.random(in: 0...100) == 5
+            if killSwitch {
+                while heap.count > 0 && sortedArray.count > 0 {
+                    let pop = heap.pop()!
+                    let arrback = sortedArray.popLast()!
+                    XCTAssert(pop == arrback)
+                    XCTAssert(heap.count == sortedArray.count)
+                    
+                }
+            }
+        }
+    }
+}
